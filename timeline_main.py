34395e3582262016d1dc5c4d3975e69d9a0858a3
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-05-05 09:49:12 +0300

    Update timeline_main.py

--- timeline_main.py
+++ timeline_main.py
@@ -106,6 +106,6 @@ if uploaded_file:
   ax.axvline(today.days, color='w', lw=1, alpha=0.7)
   ax.text(today.days, len(df)+0.5, 'Today', ha='center', color='w')
 
-  plt.savefig('gantt.png', facecolor='#36454F', dpi=300)
+  plt.savefig('gantt.png', facecolor='#36454F')
   
   st.pyplot(plt)
